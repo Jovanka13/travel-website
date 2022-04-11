3967c9251f7cb3e64b72964241736f771613cefe
Jovanka13 <jovanka.karoloska@gmail.com>
2022-04-11 22:22:27 +0200

    travel-website

---
+++ script.js
@@ -0,0 +1,34 @@
+// Preloader
+const loader = document.querySelector(".loader");
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    loader.style.display = "none";
+  }, 2000);
+});
+
+// Scroll Button
+let mybutton = document.getElementById("btn-back-to-top");
+
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
